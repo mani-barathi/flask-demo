3e4be4f0cc4208dfa27eccbd1678dd3403fb6505
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2022-01-02 16:05:13 +0530

    added forms

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
+
 f = open('data.json')
 pokemons = json.load(f)
 # /users
@@ -61,6 +62,24 @@ def about_page():
 
     print('got a request to about page')
     return '<h1> About Page</h1>'
+
+
+@app.route("/addproduct", methods=['GET', 'POST'])
+def add_product_page():
+    print('Method Type: ', request.method)
+    is_submited = False
+
+    if request.method == 'GET':
+        print('This is get request')
+
+    if request.method == 'POST':
+        print('This is post request')
+        print('form', request.form)
+        name = request.form.get('product_name')
+        is_submited = True
+        print('Submitted name is ', name)
+
+    return render_template('add_product.html', is_submited=is_submited)
 
 
 if __name__ == "__main__":
